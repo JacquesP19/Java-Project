9dad65d8cf01f1f080a5701e4a059c60725585ff
Jacques <pedrinhojcastro@gmail.com>
2016-11-28 17:46:32 -0200

    Fix 000

---
+++ ExampleJ/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+	class ExampleJ {
+		public static void main(String args[])
+			throws java.io.IOException {
+class don  { 
+boolean tof(int x) {
+	if (x%2 == 0) return true;
+		else return false;
+			}
+		}
+
+don ee = new don();
+	Scanner hh  = new Scanner(System.in);  
+		int x = hh.nextInt(), y = 1;
+			if (ee.tof(x)) System.out.println("é par");
+				else System.out.println("é ímpar");
+if (x > 0) System.out.println("é maior que zero");
+	int yx = x;
+		while(yx<=x*10) {
+			System.out.println(x +"x" + y +"="+yx);
+				y += 1;
+					yx = x * y; }
+double xx = hh.nextInt(), yy = 10;double xxyyx = xx/yy;
+	while(yy >= 1) {
+		int xyx = (int) xx, yxy = (int) yy;
+			xxyyx = xx / yy;
+				System.out.println(xyx + ":" + yxy + "=" + xxyyx);
+					--yy; }
+int xxyy = hh.nextInt(), xyxx = 1, xxyxy = (int) Math.pow(xxyy, xyxx) ;
+	while(xyxx <= 10) {
+		System.out.println(xxyy+"^"+xyxx+"="+xxyxy);
+			xxyxy = (int) Math.pow(xxyy, xyxx);
+				++xyxx;
+				} 
+     	}
+	}
+
+
+	
